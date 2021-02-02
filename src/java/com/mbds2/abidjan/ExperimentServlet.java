537540304573add1112078c1ac251cd79ba6aa85
pandorum2096 <toureabdouljunior@gmail.com>
2021-02-02 09:30:50 +0000

    Tester les propriétés des requêtes HTTP
    1-Ajoutez au fur et à mesure les fonctionnalités permettant de transformer la réponse de la servlet comme sur la figure ci-après.
    
    2-Commitez et push vos changements

--- src/java/com/mbds2/abidjan/ExperimentServlet.java
+++ src/java/com/mbds2/abidjan/ExperimentServlet.java
@@ -7,6 +7,7 @@ package com.mbds2.abidjan;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,16 +30,41 @@ public class ExperimentServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        
+        //
+        final int serverPort = request.getServerPort();
+        //
+        InetAddress IP = InetAddress.getLocalHost();
+        //
+        String MonIP = IP.getHostAddress();
+        //
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ExperimentServlet</title>");            
+            out.println("<title>Servlet MyServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ExperimentServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1 style='text-align:center'>Ma premiere Servlet</h1>");
+            out.println("<b><h2 style='text-align:left;'>Informations concernant l'adresse IP et le navigateur utilisé par le client:</h2></b>");
+            out.println("<h3 style='text-align:left'>Adresse IP du client: " + MonIP + "</h3>");
+            out.println("<h3 style='text-align:left'>Navigateur du client: " + request.getHeader("client") + " </h3>");
+            out.println("<b><h2 style='text-align:left'>Informations concernant la requete du client:</h2></b>");
+            out.println("<h3 style='text-align:left'>Methode : " + request.getMethod()  + "</h3>");
+            out.println("<h3 style='text-align:left'>Protocole : " + request.getProtocol() + " </h3>");
+            out.println("<h3 style='text-align:left'>Uri demande : " + request.getProtocol() + "</h3>");
+            out.println("<h2 style='text-align:left'>Informations concernant la requete de la requete:</h2>");
+            out.println("<h3 style='text-align:left'>host : " + String.format("%s:%s", request.getServerName(), serverPort) + "</h3>");
+            out.println("<h3 style='text-align:left'>user-agent : " + request.getHeader("user-agent") + " </h3>");
+            out.println("<h3 style='text-align:left'>accept : " + request.getHeader("accept") + "</h3>");
+            out.println("<h3 style='text-align:left'>accept-language : " + request.getHeader("accept-language") + "</h3>");
+            out.println("<h3 style='text-align:left'>accept-encoding : " + request.getHeader("Accept-Encoding")  + " </h3>");
+            out.println("<h3 style='text-align:left'>accept-charset : " + request.getHeader("Accept-Charset") + "</h3>");
+            out.println("<h3 style='text-align:left'>keep-alive : " + request.getHeader("Keep-Alive") + "</h3>");
+            out.println("<h3 style='text-align:left'>connection : " + request.getHeader("connection") + " </h3>");
+            //out.println("<h1>Servlet ExperimentServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
